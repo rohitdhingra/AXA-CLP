30f5b3f1eff4256bc8c197404512e7945b754a20
Rohit Dhingra <Rohit Dhingra@DESKTOP-TQ58HNN>
2019-05-25 15:55:58 +0530

    Entities Addition

--- CLP/src/main/java/axa/partners/entities/Customer.java
+++ CLP/src/main/java/axa/partners/entities/Customer.java
@@ -1,8 +1,14 @@
 package axa.partners.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Customer {
 
+	@Id
+	@Column(name="customer_id")
+	private Long customerId;
+	
 }
--- CLP/src/main/java/axa/partners/entities/Policy.java
+++ CLP/src/main/java/axa/partners/entities/Policy.java
@@ -1,8 +1,13 @@
 package axa.partners.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Policy {
 
+	@Id
+	@Column(name="policy_id")
+	private Long policyId;
 }
