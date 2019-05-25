9b54bf98541ad547878d1ed8fe52f1d9479276b1
Rohit Dhingra <Rohit Dhingra@DESKTOP-TQ58HNN>
2019-05-25 11:14:20 +0530

    CLP project initial setup

---
+++ CLP/src/main/java/axa/partners/ClpApplication.java
@@ -0,0 +1,13 @@
+package axa.partners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClpApplication.class, args);
+	}
+
+}
---
+++ CLP/src/test/java/axa/partners/ClpApplicationTests.java
@@ -0,0 +1,16 @@
+package axa.partners;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
